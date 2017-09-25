8c269629ab22b418b4fe264f292f97b13cad31ef
Alexandre PALO <a-palo@laposte.net>
2017-09-25 12:27:07 -0400

    first map & search bar boilerplate

--- cse-project/src/App.js
+++ cse-project/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { MapContainer, SearchBar } from './components/presentationals'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="formContainer">
+          <SearchBar />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MapContainer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
